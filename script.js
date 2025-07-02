8a374d2dc946bb89065315880a9bdf79284a3198
thebrunin <bruno.marshall22@gmail.com>
2025-07-02 20:29:47 -0300

    fromatando script

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const entrada = document.getElementById("entrada");
-    const output = document.getElementById("output");
-    const modal = document.getElementById("modal");
+const output = document.getElementById("output");
+const modal = document.getElementById("modal");
 
-    function calcularExpressao(expr) {
-      const regex = /^[0-9+\-*/().\s]+$/;
+function calcularExpressao(expr) {
+    const regex = /^[0-9+\-*/().\s]+$/;
 
-      if (!regex.test(expr)) return "❌ Expressão inválida.";
+    if (!regex.test(expr)) return "❌ Expressão inválida.";
 
-      try {
-        return `📌 Resultado: ${eval(expr)}`;
-      } catch {
-        return "❌ Erro ao calcular.";
-      }
+    try {
+    return `📌 Resultado: ${eval(expr)}`;
+    } catch {
+    return "❌ Erro ao calcular.";
+    }
+}
+
+function executarExpressao() {
+    const expr = entrada.value.trim();
+
+    if (expr.length > 0) {
+    const resultado = calcularExpressao(expr);
+    
+    output.innerText += `> ${expr}\n${resultado}\n\n`;
+    entrada.value = "";
+    output.scrollTop = output.scrollHeight;
+    }
+}
+
+entrada.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+    executarExpressao();
     }
 
-    function executarExpressao() {
-      const expr = entrada.value.trim();
-
-      if (expr.length > 0) {
-        const resultado = calcularExpressao(expr);
-        
-        output.innerText += `> ${expr}\n${resultado}\n\n`;
-        entrada.value = "";
-        output.scrollTop = output.scrollHeight;
-      }
+    if (e.key === "Delete") {
+    output.innerText = "";
+    entrada.value = "";
     }
+});
 
-    entrada.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        executarExpressao();
-      }
+function abrirModal() {
+    modal.style.display = "flex";
+}
 
-      if (e.key === "Delete") {
-        output.innerText = "";
-        entrada.value = "";
-      }
-    });
-
-    function abrirModal() {
-      modal.style.display = "flex";
-    }
-
-    function fecharModal() {
-      modal.style.display = "none";
-    }
+function fecharModal() {
+    modal.style.display = "none";
+}
