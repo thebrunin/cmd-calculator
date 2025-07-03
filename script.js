d1d76934ffd93d9acd7b1476f55b9e4db32afd7a
thebrunin <bruno.marshall22@gmail.com>
2025-07-02 21:44:33 -0300

    adicionando funcionalidade de historico de inputs

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const entrada = document.getElementById("entrada");
 const output = document.getElementById("output");
 const modal = document.getElementById("modal");
 
+const inputs = [];
+let inputIndex = 0;
+
 function calcularExpressao(expr) {
     const regex = /^[0-9+\-*/().\s]+$/;
 
     if (!regex.test(expr)) return "❌ Expressão inválida.";
 
+    inputs.push(expr);
+    inputIndex = inputs.length;
+
     try {
-    return `📌 Resultado: ${eval(expr)}`;
+        return `📌 Resultado: ${eval(expr)}`;
     } catch {
-    return "❌ Erro ao calcular.";
+        return "❌ Erro ao calcular.";
     }
 }
 
@@ -18,24 +24,42 @@ function executarExpressao() {
     const expr = entrada.value.trim();
 
     if (expr.length > 0) {
-    const resultado = calcularExpressao(expr);
-    
-    output.innerText += `> ${expr}\n${resultado}\n\n`;
-    entrada.value = "";
-    output.scrollTop = output.scrollHeight;
+        const resultado = calcularExpressao(expr);
+        
+        output.innerText += `> ${expr}\n${resultado}\n\n`;
+        entrada.value = "";
+        output.scrollTop = output.scrollHeight;
     }
 }
 
 entrada.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-    executarExpressao();
+        executarExpressao();
     }
 
     if (e.key === "Delete") {
-    output.innerText = "";
-    entrada.value = "";
+        output.innerText = "";
+        entrada.value = "";
+    }
+
+    if (e.key === "ArrowUp") {
+        inputAnterior();
+    }
+
+    if (e.key === "ArrowDown") {
+        inputPosterior();
     }
 });
+
+function inputAnterior() {
+    if(inputIndex > 0)
+        entrada.value = inputs[--inputIndex];
+}
+
+function inputPosterior() {
+    if(inputIndex < (inputs.length - 1))
+        entrada.value = inputs[++inputIndex];
+}
 
 function abrirModal() {
     modal.style.display = "flex";
